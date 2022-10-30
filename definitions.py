d9e26e9c0c55c0e7695790907d80cf8d673cbcd1
Seth Bryant <bryant.seth@gmail.com>
2022-10-30 12:55:28 +0100

    Create definitions.py

---
+++ definitions.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar. 25, 2022
+
+@author: cefect
+'''
+import os
+proj_dir = r'C:\LS\09_REPOS\02_JOBS\2210_AggFSyn'
+src_dir = proj_dir
+src_name='aggF'
+
+ 
+
+logcfg_file=r'C:\LS\09_REPOS\01_COMMON\coms\logger.conf'
+
+root_dir=r'C:\LS\10_IO\2112_Agg'
+wrk_dir=root_dir
+ 
